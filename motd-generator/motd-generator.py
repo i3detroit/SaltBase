9e20405dc83517cf53f9dd9565a9a5952de72063
Amelia Meyer <agmlego@gmail.com>
2021-10-30 22:02:21 -0400

    Initial script and associated for MOTD generation.

---
+++ motd-generator/motd-generator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import configparser
+import os
+import os.path
+import sys
+
+from pyfiglet import Figlet
+
+if __name__ == '__main__':
+    motd_path = sys.argv[1]
+    config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'hosts.ini'))
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    fig = Figlet(font=config.get('figlet', 'font'))
+    for section in config.sections():
+        if '_domain' not in section:
+            continue
+        domain = section.replace('_domain', '')
+        for hostname in config[section]:
+            description = config[section][hostname]
+            host_fig = fig.renderText(f'{hostname}.{domain}')
+            center_w = max(map(len, host_fig.split('\n')))
+            print(f'{host_fig}{description.center(center_w)}\n')

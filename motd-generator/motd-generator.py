cdc890a43faa47c4440e6c5c6264fd48de2394e3
Amelia Meyer <agmlego@gmail.com>
2021-10-30 22:16:13 -0400

    Write MOTDs to files in specified directory

--- motd-generator/motd-generator.py
+++ motd-generator/motd-generator.py
@@ -9,7 +9,8 @@ from pyfiglet import Figlet
 
 if __name__ == '__main__':
     motd_path = sys.argv[1]
-    config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'hosts.ini'))
+    config_path = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '..', 'hosts.ini'))
     config = configparser.ConfigParser()
     config.read(config_path)
     fig = Figlet(font=config.get('figlet', 'font'))
@@ -21,4 +22,5 @@ if __name__ == '__main__':
             description = config[section][hostname]
             host_fig = fig.renderText(f'{hostname}.{domain}')
             center_w = max(map(len, host_fig.split('\n')))
-            print(f'{host_fig}{description.center(center_w)}\n')
+            with open(os.path.join(motd_path, f'{hostname}.motd'), 'w') as f:
+                f.write(f'{host_fig}{description.center(center_w)}\n')
